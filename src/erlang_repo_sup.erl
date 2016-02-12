ed18d2a7348ec0e90755f3cad1e2c6c899fff4cc
Euen <euenlopez@gmail.com>
2016-02-12 17:32:00 -0300

    making it fail to compiler tool

---
+++ src/erlang_repo_sup.erl
@@ -0,0 +1,12 @@
+-module(erlang_repo_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+    UnUsedVar = nothing,
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    Procs = [],
+    {ok, {{one_for_one, 1, 5}, Procs}}.
