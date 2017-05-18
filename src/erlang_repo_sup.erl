79abe12023e3882375c92e02a90dfff865de5ab4
Euen <euenlopez@gmail.com>
2017-05-18 09:25:27 -0700

    dialyzer fail common, elvis fail, compile fail common, xref fail

--- src/erlang_repo_sup.erl
+++ src/erlang_repo_sup.erl
@@ -1,17 +1,13 @@
--module(erlang_repo_sup).
+-module(erlang_repo_sp).
 -behaviour(supervisor).
 
 -export([start_link/0, init/1]).
 
 -spec start_link() -> ok.
 start_link() ->
-    % tooooo loooooooooong lineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
     % UnUsedVar = nothing,
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
     Procs = [],
     {ok, {{one_for_one, 1, 5}, Procs}}.
-
-not_used() ->
-  nothing.
